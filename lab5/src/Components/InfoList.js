882408b2a35fa9097e2716479280b2f9fe6dbc6a
Rigel Chan <rigel_chan@hotmail.com>
2022-07-10 23:16:42 -0600

    ss

--- lab5/src/Components/InfoList.js
+++ lab5/src/Components/InfoList.js
@@ -7,14 +7,24 @@ export default class InfoList extends React.Component {
     albums: []
   }
 
+
   componentDidMount() {
     axios.get(`https://jsonplaceholder.typicode.com/photos`)
       .then(res => {
         const albums = res.data;
         this.setState({ albums });
       })
+      
   }
- 
+
+  deleteRow(id, e){
+    axios.delete('https://jsonplaceholder.typicode.com/photos/${id}')
+    .then(res => {
+      const albums =  this.state.albums.filter(item => item.id !== id);;
+      this.setState({ albums });
+    })
+  }
+
 
   render() {
     return (
@@ -25,7 +35,7 @@ export default class InfoList extends React.Component {
               <tr><td key={albums.id}>{albums.id}</td>
               <td key={albums.id}>{albums.title}</td>
               <td><img key={albums.id} src={albums.thumbnailUrl} alt="test"/></td>
-              <td style={{width:"100px"}}><button key={albums.id} onClick={axios.delete(`https://jsonplaceholder.typicode.com/photos/${albums.id}`)}>Delete</button></td>
+              <td style={{width:"100px"}}><button key={albums.id} onClick={(e)=>this.deleteRow(albums.id,e)}>Delete</button></td>
               </tr>
             )
         }
